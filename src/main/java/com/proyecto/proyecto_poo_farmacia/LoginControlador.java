c5076224fdd3ffc3be247c96c38ff4c5a18d9582
Orland H <24heyerunsu@gmail.com>
2023-08-18 11:10:18 -0500

    Inicio Dibujar Interfaz

--- src/main/java/com/proyecto/proyecto_poo_farmacia/LoginControlador.java
+++ src/main/java/com/proyecto/proyecto_poo_farmacia/LoginControlador.java
@@ -3,12 +3,6 @@ package com.proyecto.proyecto_poo_farmacia;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
-    @FXML
-    private Label welcomeText;
+public class LoginControlador {
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
 }
